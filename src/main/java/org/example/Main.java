f98b30d521e12cd01d0a9c464adee20b44be902d
Gareth <garethshippen@yahoo.co.uk>
2022-12-11 22:12:06 +0000

    Working

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,70 @@
+package org.example;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) throws URISyntaxException {
+        File source = getFile("plaintext.txt");
+        File sink = getFile("cyphertext.txt");
+
+        createCypherText(source, sink);
+        System.out.println("Finished.");
+    }
+    private static char swapChar(char _c, int offset)
+    {
+        final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int index = alphabet.indexOf(_c);
+        return alphabet.charAt((index + offset)%alphabet.length());
+    }
+
+    private static char swapChar(char _c)
+    {
+        return swapChar(_c, 13);
+    }
+
+    private static File getFile(String _fileName) throws URISyntaxException {
+        ClassLoader classLoader = Main.class.getClassLoader();
+        URL resource = classLoader.getResource(_fileName);
+        if(resource == null)
+        {
+            throw new IllegalArgumentException("File not found. " + _fileName);
+        }
+        else
+        {
+            return new File(resource.toURI());
+        }
+    }
+
+    private static void createCypherText(File _file, File _output)
+    {
+        BufferedReader reader;
+        BufferedWriter writer;
+        try
+        {
+            reader = new BufferedReader(new FileReader(_file));
+            writer = new BufferedWriter(new FileWriter(_output));
+            int red;
+            char character;
+            while((red = reader.read()) != -1)
+            {
+                character = Character.toLowerCase((char) red);
+                red = (int)character;
+                //a = 97
+                //z = 122
+                if(red > 96 && red < 123) {
+                    character = swapChar(character);
+                }
+                writer.write((int)character);
+
+            }
+            reader.close();
+            writer.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
